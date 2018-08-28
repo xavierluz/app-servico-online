149131fc1f71fef5b2ae2bffdce4429437e9e3d1
Celso Xavier Luz <xavierluz@gmail.com>
2018-08-28 16:59:54 -0300

    Animações

--- src/app/servico/servico-detalhe/servico-detalhe.component.ts
+++ src/app/servico/servico-detalhe/servico-detalhe.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations'
 import { ServicoModel } from '../model/servico.model';
 
 @Component({
   selector: 'app-servico-detalhe',
   templateUrl: './servico-detalhe.component.html',
+  animations: [
+    trigger('servicoAppeared', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', [
+        style({ opacity: 0, transform: 'translateY(-20px)' }),
+        animate('800ms 0s ease-in')
+      ])
+    ])
+  ],
   styleUrls: ['./servico-detalhe.component.css']
 })
 export class ServicoDetalheComponent implements OnInit {
+  servicoEstado = 'ready';
   @Input() servico:ServicoModel;
   constructor() { }
 
--- src/app/tipo-servico/model/tipo-servico-model/tipo-servico-model.component.ts
+++ src/app/tipo-servico/model/tipo-servico-model/tipo-servico-model.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TipoServicoModel } from '../tipo.servico.model';
-
+import {trigger, state,style,transition, animate} from '@angular/animations'
 @Component({
   selector: 'app-tipo-servico-model',
   templateUrl: './tipo-servico-model.component.html',
+  animations:[
+    trigger('tipoServicoAppeared',[
+      state('ready',style({opacity:1})),
+      transition('void => ready',[
+        style({opacity:0,transform: 'translate(-30px, -10px)'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ],
   styleUrls: ['./tipo-servico-model.component.css']
 })
 export class TipoServicoModelComponent implements OnInit {
+  tipoServicoEstado='ready';
   @Input() tipoServicoModel:TipoServicoModel;
   constructor() { }
 
